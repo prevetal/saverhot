ac1fbbd184b7125091dd92ef4f2a9b2e53feb6c7
Vitaliy Sulimenko <prevetal@gmail.com>
2023-08-01 13:19:55 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,164 @@
+// Ширина окна для ресайза
+WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+BODY = document.getElementsByTagName('body')[0]
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	// Слайдер в блоке - "О компании"
+	let aboutDataSlider = document.querySelector('.about_data .swiper')
+
+	if (aboutDataSlider) {
+		new Swiper('.about_data .swiper', {
+			loop: true,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 24,
+			slidesPerView: 1,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			}
+		})
+	}
+
+
+	// Карусель товаров
+	const reviewsSliders = [],
+		reviews = document.querySelectorAll('.reviews .swiper')
+
+	reviews.forEach(function (el, i) {
+		el.classList.add('reviews_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			breakpoints: {
+				0: {
+					spaceBetween: 10,
+					slidesPerView: 1
+				},
+				768: {
+					spaceBetween: 20,
+					slidesPerView: 2
+				},
+				1024: {
+					spaceBetween: 20,
+					slidesPerView: 3
+				}
+			},
+			on: {
+				init: swiper => setHeight(swiper.el.querySelectorAll('.review')),
+				resize: swiper => {
+					let reviews = swiper.el.querySelectorAll('.review')
+
+					reviews.forEach(el => el.style.height = 'auto')
+
+					setHeight(reviews)
+				}
+			}
+		}
+
+		reviewsSliders.push(new Swiper('.reviews_s' + i, options))
+	})
+
+
+	// Аккордион
+	$('body').on('click', '.accordion .accordion_item .head', function(e) {
+		e.preventDefault()
+
+		const $item = $(this).closest('.accordion_item'),
+			$accordion = $(this).closest('.accordion')
+
+		if ($item.hasClass('active')) {
+			$item.removeClass('active').find('.data').slideUp(300)
+		} else {
+			$accordion.find('.accordion_item').removeClass('active')
+			$accordion.find('.data').slideUp(300)
+
+			$item.addClass('active').find('.data').slideDown(300)
+		}
+	})
+
+
+	// Моб. меню
+	$('header .mob_menu_btn').click((e) => {
+		e.preventDefault()
+
+		$('header .mob_menu_btn').addClass('active')
+		$('body').addClass('menu_open')
+		$('.mob_menu').addClass('show')
+		$('.overlay').fadeIn(300)
+	})
+
+	$('.mob_menu .close_btn, .overlay').click((e) => {
+		e.preventDefault()
+
+		$('header .mob_menu_btn').removeClass('active')
+		$('body').removeClass('menu_open')
+		$('.mob_menu').removeClass('show')
+		$('.overlay').fadeOut(300)
+	})
+})
+
+
+
+window.addEventListener('resize', function () {
+	WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+
+	let windowW = window.outerWidth
+
+	if (typeof WW !== 'undefined' && WW != windowW) {
+		// Перезапись ширины окна
+		WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+
+
+		// Моб. версия
+		if (!fakeResize) {
+			fakeResize = true
+			fakeResize2 = false
+
+			document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
+		}
+
+		if (!fakeResize2) {
+			fakeResize2 = true
+
+			if (windowW < 360) document.getElementsByTagName('meta')['viewport'].content = 'width=360, user-scalable=no'
+		} else {
+			fakeResize = false
+			fakeResize2 = true
+		}
+	}
+})
