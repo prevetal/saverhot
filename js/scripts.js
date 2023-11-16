7c2d645c6c29361c9c9e5362381e9288bbc85e21
Vitaliy Sulimenko <prevetal@gmail.com>
2023-11-16 21:11:09 +0100

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 
-	// Карусель товаров
+	// Карусель отзывов
 	const reviewsSliders = [],
 		reviews = document.querySelectorAll('.reviews .swiper')
 
@@ -94,6 +94,132 @@ document.addEventListener('DOMContentLoaded', function () {
 	})
 
 
+	// Этапы производства
+	$('.production_steps .step .head .desc_spoler_btn').click(function(e) {
+		e.preventDefault()
+
+		let step = $(this).closest('.step')
+
+		$(this).toggleClass('active')
+		step.find('.head .desc_wrap').slideToggle(300)
+	})
+
+
+	const productionStepsSliders = [],
+		productionSteps = document.querySelectorAll('.production_steps .swiper')
+
+	productionSteps.forEach(function (el, i) {
+		el.classList.add('production_steps_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			breakpoints: {
+				0: {
+					spaceBetween: 10,
+					slidesPerView: 1
+				},
+				768: {
+					spaceBetween: 20,
+					slidesPerView: 2
+				}
+			}
+		}
+
+		productionStepsSliders.push(new Swiper('.production_steps_s' + i, options))
+	})
+
+
+	// История компании
+	const historySliders = [],
+		history = document.querySelectorAll('.history .swiper')
+
+	history.forEach(function (el, i) {
+		el.classList.add('history_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			spaceBetween: 20,
+			slidesPerView: 'auto'
+		}
+
+		historySliders.push(new Swiper('.history_s' + i, options))
+	})
+
+
+	// История компании
+	const gallerySliders = [],
+		gallery = document.querySelectorAll('.gallery .swiper')
+
+	gallery.forEach(function (el, i) {
+		el.classList.add('gallery_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			breakpoints: {
+				0: {
+					spaceBetween: 20,
+					slidesPerView: 'auto'
+				},
+				768: {
+					spaceBetween: 20,
+					slidesPerView: 3
+				},
+				1024: {
+					spaceBetween: 20,
+					slidesPerView: 4
+				}
+			}
+		}
+
+		gallerySliders.push(new Swiper('.gallery_s' + i, options))
+	})
+
+
 	// Аккордион
 	$('body').on('click', '.accordion .accordion_item .head', function(e) {
 		e.preventDefault()
@@ -124,6 +250,31 @@ document.addEventListener('DOMContentLoaded', function () {
 		MODAL: "Вы можете закрыть это модальное окно нажав клавишу ESC"
 	}
 
+	Fancybox.defaults.template = {
+		closeButton: '<svg><use xlink:href="images/sprite.svg#ic_close"></use></svg>',
+		spinner: '<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="25 25 50 50" tabindex="-1"><circle cx="50" cy="50" r="20"/></svg>',
+		main: null
+	}
+
+
+	// Всплывающие окна
+	const modalBtns = document.querySelectorAll('.modal_btn')
+
+	if (modalBtns) {
+		modalBtns.forEach(el => {
+			el.addEventListener('click', e => {
+				e.preventDefault()
+
+				Fancybox.close()
+
+				Fancybox.show([{
+					src: document.getElementById(el.getAttribute('data-modal')),
+					type: 'inline'
+				}])
+			})
+		})
+	}
+
 
 	// Увеличение картинки
 	Fancybox.bind('.fancy_img', {
@@ -133,6 +284,26 @@ document.addEventListener('DOMContentLoaded', function () {
 		Thumbs: {
 			autoStart: false,
 		}
+	})
+
+
+	// Модальные окна
+	$('.custom_modal_btn').click(function (e) {
+		e.preventDefault()
+
+		let modal = $(this).data('modal')
+
+		$('body').addClass('menu_open')
+		$('#' + modal).addClass('show')
+		$('.overlay').fadeIn(300)
+	})
+
+
+	// Форма - поле ввода
+	$('.form .input').keyup(function () {
+		$(this).val().length
+			? $(this).addClass('active')
+			: $(this).removeClass('active')
 	})
 
 
@@ -146,13 +317,85 @@ document.addEventListener('DOMContentLoaded', function () {
 		$('.overlay').fadeIn(300)
 	})
 
-	$('.mob_menu .close_btn, .overlay').click((e) => {
+	$('.mob_menu .close_btn, .overlay, #search_modal .close_btn, .custom_modal .close_btn').click((e) => {
 		e.preventDefault()
 
 		$('header .mob_menu_btn').removeClass('active')
 		$('body').removeClass('menu_open')
 		$('.mob_menu').removeClass('show')
 		$('.overlay').fadeOut(300)
+		$('#search_modal, .custom_modal').removeClass('show')
+	})
+
+
+	// Поиск
+	$('.search .btn').click(e => {
+		e.preventDefault()
+
+		$('#search_modal').toggleClass('show')
+		$('.overlay').fadeIn(300)
+
+		setTimeout(() => $('#search_modal .input').focus())
+	})
+
+
+	// Маска ввода
+	const phoneInputs = document.querySelectorAll('input[type=tel]')
+
+	if (phoneInputs) {
+		phoneInputs.forEach(el => {
+			IMask(el, {
+				mask: '+{7} (000) 000-00-00',
+				lazy: true
+			})
+		})
+	}
+
+
+	if (is_touch_device()) {
+		const subMenus = document.querySelectorAll('header .menu .sub_menu')
+
+		// Подменю на тач скрине
+		$('header .menu_item > a.sub_link').addClass('touch_link')
+
+		$('header .menu_item > a.sub_link').click(function (e) {
+			const $dropdown = $(this).next()
+
+			if ($dropdown.css('visibility') === 'hidden') {
+				e.preventDefault()
+
+				subMenus.forEach(el => el.classList.remove('show'))
+				$dropdown.addClass('show')
+
+				BODY.style = 'cursor: pointer;'
+			}
+		})
+
+		// Закрываем под. меню при клике за её пределами
+		document.addEventListener('click', e => {
+			if ($(e.target).closest('.menu').length === 0) {
+				subMenus.forEach(el => el.classList.remove('show'))
+
+				BODY.style = 'cursor: default;'
+			}
+		})
+	}
+
+
+	// Моб. меню
+	$('.mob_menu .menu a.sub_link').click(function(e) {
+		e.preventDefault()
+
+		let sub = $(this).data('sub')
+
+		$('.mob_menu .sub_menu').removeClass('show')
+		$('.mob_menu ' + sub).addClass('show')
+	})
+
+	$('.mob_menu .sub_menu .back a').click(function(e) {
+		e.preventDefault()
+
+		$(this).closest('.sub_menu').removeClass('show')
 	})
 })
 
